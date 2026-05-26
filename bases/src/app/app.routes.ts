f87600e5eb47a58c26465e655c5cd750610c3b0e
JavierLanauGomez <j.lanau.gomez@gmail.com>
2026-05-26 12:10:49 +0200

    c

--- bases/src/app/app.routes.ts
+++ bases/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 
 import { Routes } from '@angular/router';
 import { CounterpageComponent } from './pages/counter/Counter-page.component';
-import { HeroPageComponent } from './pages/hero/Hero-Page.Component';
+
 
 
 
