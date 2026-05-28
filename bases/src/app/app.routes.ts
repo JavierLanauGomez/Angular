a297169f0195ab32ceee27ad0f626bb35f8251c1
JavierLanauGomez <j.lanau.gomez@gmail.com>
2026-05-28 09:35:48 +0200

    Creamos Componente  e inicio parte visual

--- bases/src/app/app.routes.ts
+++ bases/src/app/app.routes.ts
@@ -2,7 +2,7 @@
 import { Routes } from '@angular/router';
 import { CounterpageComponent } from './pages/counter/counter.page.components';
 import { HeroPageComponent } from './pages/hero/hero-page.components';
-
+import { dragonballPageComponent } from './pages/components/dragonball/dragonball-PageComponent';
 
 export const routes: Routes = [
 {
@@ -14,6 +14,10 @@ component: CounterpageComponent
     path: 'hero',
     component: HeroPageComponent
   },
+  {
+  path: 'dragonball',
+  component: dragonballPageComponent
+},
   {
     path: '**',
     redirectTo: ''
---
+++ bases/src/app/pages/components/dragonball/dragonball-PageComponent.ts
@@ -0,0 +1,16 @@
+
+
+import { Component } from '@angular/core';
+
+
+
+@Component({
+
+  templateUrl: './dragonball-PageComponent.html',
+
+})
+export class dragonballPageComponent {
+
+
+}
+
