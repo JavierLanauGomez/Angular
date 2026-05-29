266e19dc3aff7e76dc293d1ab98da88dac038352
JavierLanauGomez <j.lanau.gomez@gmail.com>
2026-05-29 11:09:32 +0200

    uso de if y css en dragonball

--- bases/src/app/pages/components/dragonball/dragonball-PageComponent.ts
+++ bases/src/app/pages/components/dragonball/dragonball-PageComponent.ts
@@ -1,15 +1,35 @@
 
 
 import { Component } from '@angular/core';
+import { signal } from '@angular/core';
 
+
+interface Character {
+
+  id: number;
+  name: string;
+  powerLevel: number;
+
+}
 
 
 @Component({
-
+  standalone: true,
   templateUrl: './dragonball-PageComponent.html',
 
 })
 export class dragonballPageComponent {
+
+  characters = signal (<Character[]>([
+
+    { id: 1,name: 'Goku',powerLevel: 9001},
+    { id: 2,name: 'Vegeta',powerLevel: 8500},
+    { id: 3,name: 'Yamcha',powerLevel: 7000}
+
+
+
+  ]));
+
 
 
 }
